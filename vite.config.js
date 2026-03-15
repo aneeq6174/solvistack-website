dbc793a2a432a8e4f0776e11a35d2874a13139c9
Aneeq Ur Rehman <aneeqijaz99@gmail.com>
2026-03-16 02:04:24 +0500

    added sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://solvistack.com'
+    })
+  ],
 })
